33e5efd98001bdaeece56cd7383a976c60d0b514
Shivam Singh <shiv41297@gmail.com>
2024-08-13 16:01:20 -0400

    chore: Update server port to 4000

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(BASE_ROUTE, chartsRoutes);
-app.listen(process.env.PORT, () => {
+app.listen(4000, () => {
   console.clear();
   console.log(`Server running on port ${process.env.PORT}`);
 });
